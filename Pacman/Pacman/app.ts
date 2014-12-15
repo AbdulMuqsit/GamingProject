47b0fbb5ee5b80bde497e686109e6ad5d3733ca7
AbdulMuqsit <abdemuqsit@hotmail.com>
2014-12-15 06:12:47 +0500

    Fixed

---
+++ Pacman/Pacman/app.ts
@@ -0,0 +1,27 @@
+﻿class Pacman {
+
+    constructor() {
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
+    }
+
+    game: Phaser.Game;
+
+    preload() {
+        this.game.load.tilemap('maze', 'assets/some.json', null, Phaser.Tilemap.TILED_JSON);
+        this.game.load.image('sprites', 'assets/sprites.png');
+    }
+
+    create() {
+        var map = this.game.add.tilemap('maze');
+        map.addTilesetImage('sprites', 'sprites');
+        var layer = map.createLayer('Back');
+        layer.resizeWorld();
+    }
+
+}
+
+window.onload = () => {
+
+    var game = new Pacman();
+
+};
