b8e8d2822400349375e2ae3deea501fe985f4938
AbdulMuqsit <abdemuqsit@hotmail.com>
2014-12-17 23:13:56 +0500

    change to 32

--- Pacman.TypeScript/Pacman/app.js
+++ Pacman.TypeScript/Pacman/app.js
@@ -1,27 +1,22 @@
-﻿class Pacman {
-
-    constructor() {
+﻿var Pacman = (function () {
+    function Pacman() {
         this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
     }
-
-    game: Phaser.Game;
-
-    preload() {
+    Pacman.prototype.preload = function () {
         this.game.load.tilemap('maze', 'assets/some.json', null, Phaser.Tilemap.TILED_JSON);
         this.game.load.image('pacman', 'assets/pacman.png');
-    }
+    };
 
-    create() {
+    Pacman.prototype.create = function () {
         var map = this.game.add.tilemap('maze');
         map.addTilesetImage('pacman', 'pacman');
         var layer = map.createLayer('Tile');
         layer.resizeWorld();
-    }
+    };
+    return Pacman;
+})();
 
-}
-
-window.onload = () => {
-
+window.onload = function () {
     var game = new Pacman();
-
 };
+//# sourceMappingURL=app.js.map
